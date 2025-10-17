f156b08f2fd99631ac427b663b7087303a6b4b88
maggallo28 <maggallo784@gmail.com>
2025-10-17 14:50:07 +0100

    5 feito e 6 metade

--- Projeto1 copy/projeto1Final.py
+++ Projeto1 copy/projeto1Final.py
@@ -16,15 +16,16 @@ def conta_ocorrencias(matriz):
     return listaContador, alfabetos
 
 def grafico(data, varNames):
+    
     plt.subplots(3, 2, figsize=(10, 10))
 
     for i in range(len(varNames) - 1):
         plt.subplot(3, 2, i + 1)
-        plt.scatter(data[varNames[i]], data['MPG'], c="#C50465")
+        plt.scatter(data[varNames[i]], data['MPG'], c="#C50404")
         plt.title(f"MPG vs {varNames[i]}")
         plt.xlabel(varNames[i])
         plt.ylabel('MPG')
-        
+
         # serve para aparecerem so inteiros no grafico
         plt.gca().xaxis.set_major_locator(plt.MaxNLocator(integer=True))
         plt.gca().yaxis.set_major_locator(plt.MaxNLocator(integer=True))
@@ -34,20 +35,51 @@ def grafico(data, varNames):
     plt.show()
 
 def grafico_barras(varNames, listaContador):
-    plt.subplots(3, 2, figsize=(10, 10))
 
-    for i in range(len(varNames)):
+    for i in range(len(listaContador)):
+        lista_x_valor = list(listaContador[i].keys())
+        lista_y_contagem = list(listaContador[i].values())
 
-        #for c,v in listaContador(c,v):
+        valores_string = [str(v) for v in lista_x_valor]
 
-        
-        
+        plt.bar(valores_string, lista_y_contagem, color="#1f77b4")
+        plt.title(f"Gráfico de Barras - {varNames[i]}")
         plt.xlabel(varNames[i])
         plt.ylabel('Count')
+        plt.xticks(valores_string)
+
+        # Mostra no máximo 12 valores no eixo X (para nao haver sobre posicao de valores no grafico)
+        if len(valores_string) > 12:
+            max_elementos = len(valores_string) // 12 
+            plt.xticks(valores_string[::max_elementos])
+        else:
+            plt.xticks(valores_string)
+
+
+        plt.tight_layout()
+        plt.show()
         
-        # serve para aparecerem so inteiros no grafico
-        plt.gca().xaxis.set_major_locator(plt.MaxNLocator(integer=True))
-        plt.gca().yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+def binning(data, coluna, bins):
+    
+    for minimo, maximo in bins:
+        # Selecionar todos os valores do intervalo
+        indices_intervalo = []
+        for i in range(len(data[coluna])):
+            if data.loc[i, coluna] >= minimo and data.loc[i, coluna] <= maximo:
+                indices_intervalo.append(i)
+        
+        if indices_intervalo:
+            # Obter valores do intervalo
+            valores = [data.loc[i, coluna] for i in indices_intervalo]
+            # Calcular o valor mais representativo
+            valor_mais_representativo = max(set(valores), key=valores.count)
+            
+            # Substituir todos os valores do intervalo pela o valor mais representativo
+            for i in indices_intervalo:
+                data.loc[i, coluna] = valor_mais_representativo
+    
+    return data
+        
 
 def main():
     path = '/Users/manuelgallo/Documents/Universidade/2º ANO/TI/Projeto1/CarDataset.xlsx'
@@ -57,9 +89,26 @@ def main():
     varNames = data.columns.values.tolist()
 
     grafico(data, varNames)
-    grafico_barras(varNames, listaContador)
 
     listaContador, alfabeto = conta_ocorrencias(matriz)
+
+    grafico_barras(varNames, listaContador)
+
+    bin_weight = [(0, 2000), (2001, 3000), (3001, 4000), (4001, 5500)]
+    bin_disp   = [(0, 100), (101, 200), (201, 300), (301, 500)]
+    bin_hp     = [(0, 75), (76, 125), (126, 175), (176, 250), (251, 500)]
+
+    data = binning(data, "Weight", bin_weight)
+    data = binning(data, "Displacement", bin_disp)
+    data = binning(data, "Horsepower", bin_hp)
+
+    grafico(data, varNames)
+
+    colunas_bin = ["Weight", "Displacement", "Horsepower"]
+    matriz_bin = data[colunas_bin].values.tolist()
+    listaContador_bin, _ = conta_ocorrencias(matriz_bin)
+
+    grafico_barras(colunas_bin, listaContador_bin)
 
     return listaContador, alfabeto
 
